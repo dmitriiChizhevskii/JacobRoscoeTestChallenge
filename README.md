bb9a65942e92e5b1ac886082514885a86ab4d83f
Chizhevskiy Dmitriy <chizhevskiydmitriy@MacBook-Pro-Chizhevskiy.local>
2023-01-12 15:51:25 +0300

    add e2e tests

--- README.md
+++ README.md
@@ -26,12 +26,6 @@ $ npm run start
 ## Test
 
 ```bash
-# unit tests
-$ npm run test
-
 # e2e tests
 $ npm run test:e2e
-
-# test coverage
-$ npm run test:cov
 ```
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ValidationPipe } from '@nestjs/common';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
@@ -12,14 +13,59 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it('/todos (POST)', () => {
+  it('/todos (POST) 200', () => {
     return request(app.getHttpServer())
       .post('/todos')
       .send({ task: 'todo1' })
       .expect(200)
-      .expect({"success":true});
+      .expect({ "success": true });
+  });
+
+  it('/todos (POST) 400', () => {
+    return request(app.getHttpServer())
+      .post('/todos')
+      .send({ foo: 'bar' })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: [ 'task must be a string', 'task should not be empty' ],
+        error: 'Bad Request'
+      });
+  });
+
+  it('/todos/:id (PATCH) 404', () => {
+    return request(app.getHttpServer())
+      .post('/todos/-1')
+      .send({ updated: true })
+      .expect(404)
+      .expect({
+        statusCode: 404,
+        message: 'Cannot POST /todos/-1',
+        error: 'Not Found'
+      });
+  });
+
+  it('/subscribe (POST) 400', async () => {
+    return request(app.getHttpServer())
+      .post('/subscribe')
+      .send({ url: 'todo1' })
+      .expect(400)
+      .expect({
+        statusCode: 400,
+        message: [ 'Url is not valid.' ],
+        error: 'Bad Request'
+      });
+  });
+
+  it('/subscribe (POST) 200', async () => {
+    return request(app.getHttpServer())
+      .post('/subscribe')
+      .send({ url: 'www.foo.bar' })
+      .expect(200)
+      .expect({ "success": true });
   });
 });
